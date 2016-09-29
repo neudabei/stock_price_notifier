5fd6254499ffcb7d2b8fb917aa00804daa08430a
neudabei <robert.schultze-jena@futurelearn.com>
2016-09-29 22:15:14 +0100

    Write StockInformation class that retrieves information about a stock using the HTTParty gem and a normal HTTP get request and provides a method to get the last trade price

--- Gemfile
+++ Gemfile
@@ -2,3 +2,4 @@ source 'https://rubygems.org'
 
 gem 'stock_quote'
 gem 'pry'
+gem 'HTTParty'
---
+++ get_stock_price.rb
@@ -0,0 +1,30 @@
+class StockInformation
+  require 'HTTParty'
+
+  def initialize(symbol)
+    @symbol = symbol
+    @base_uri = base_uri = "http://query.yahooapis.com/v1/public/yql?q=select%20*%20from%20yahoo.finance.quotes%20where%20symbol%20IN%20(%22#{@symbol}%22)&format=json&env=http://datatables.org/alltables.env"
+  end
+
+  def last_trade_price
+    json_stock_information_quote["LastTradePriceOnly"].to_f
+  end
+
+  private
+
+  def yahoo_response
+    HTTParty.get(@base_uri)
+  end
+
+  def yahoo_response_body
+    yahoo_response.body
+  end
+
+  def json_stock_information
+    JSON.parse(yahoo_response_body)
+  end
+
+  def json_stock_information_quote
+    json_stock_information["query"]["results"]["quote"]
+  end
+end
