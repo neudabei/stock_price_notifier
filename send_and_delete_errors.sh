56677fcab2706b2a0b913379316e5dbe37170cf2
neudabei <robert.schultze-jena@futurelearn.com>
2016-10-01 12:02:30 +0100

    modify bash script to print out the errors before deleting it

--- send_and_delete_errors.sh
+++ send_and_delete_errors.sh
@@ -2,6 +2,6 @@ errors_file=/home/pi/ruby_scripts/stock_notifications/stock_quote_errors.txt
 
 if [ -f $errors_file ]
 then
-    cat $errors_file
+    echo `cat $errors_file`
     rm  $errors_file
 fi
