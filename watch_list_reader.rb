aa38c2f6377257c25b5215d10555f330764ff474
neudabei <robert.schultze-jena@futurelearn.com>
2016-10-01 16:28:06 +0100

    make path to yaml file relative

--- watch_list_reader.rb
+++ watch_list_reader.rb
@@ -4,6 +4,6 @@ class WatchListReader
   attr_reader :watch_list
 
   def initialize
-    @watch_list = YAML.load_file('investment_watch_list.yml')
+    @watch_list = YAML.load_file("#{__dir__}/investment_watch_list.yml")
   end
 end
