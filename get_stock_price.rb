1b88ada824d58400046c680e80ac3e25c4d861c4
neudabei <robert.schultze-jena@futurelearn.com>
2016-10-09 18:51:36 +0100

    fix typo by removing superfluous local variable

--- get_stock_price.rb
+++ get_stock_price.rb
@@ -3,7 +3,7 @@ class StockInformation
 
   def initialize(symbol)
     @symbol = symbol
-    @base_uri = base_uri = "http://query.yahooapis.com/v1/public/yql?q=select%20*%20from%20yahoo.finance.quotes%20where%20symbol%20IN%20(%22#{@symbol}%22)&format=json&env=http://datatables.org/alltables.env"
+    @base_uri = "http://query.yahooapis.com/v1/public/yql?q=select%20*%20from%20yahoo.finance.quotes%20where%20symbol%20IN%20(%22#{@symbol}%22)&format=json&env=http://datatables.org/alltables.env"
   end
 
   def last_trade_price
